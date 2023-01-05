ab974e2cbb8d82eea9df789baaa9c2f7fc17ea6d
Cezary Wal <cezary.wal@rrobotics.co>
2023-01-05 09:57:30 +0100

    initial commit

---
+++ src/main/java/com/cdq/taskprocessor/TaskProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.cdq.taskprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskProcessorApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TaskProcessorApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/cdq/taskprocessor/TaskProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdq.taskprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskProcessorApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
